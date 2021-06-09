de2299196aa111f5653c5735ef2222db408b9383
Ty Day <tyrday@gmail.com>
2021-06-09 17:02:48 -0400

    authorization added

--- server.js
+++ server.js
@@ -3,7 +3,11 @@ const connectDB = require('./config/db');
 
 const app = express();
 
+// Connect Database
 connectDB();
+
+// Init Middleware
+app.use(express.json({ extended: false }));
 
 app.get('/', (req, res) => res.send('API RUNNING'));
 
