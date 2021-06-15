530061986497e5604cff6b3757bd845acbe52502
Ty Day <tyrday@gmail.com>
2021-06-15 09:26:31 -0400

    Add Delete profile

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -141,4 +141,26 @@ router.get('/user/:user_id', async (req, res) => {
   }
 });
 
+// @route   Delete api/profile
+// @ desc   Delete profile, user and posts
+// @access  Private
+router.delete('/', auth, async (req, res) => {
+  console.log(req.user);
+  try {
+    // @todo - remove users posts
+
+    // Remove profile
+    console.log(req.user.id);
+    await Profile.findOneAndRemove({ user: req.user.id });
+
+    //Remove user
+
+    await User.findOneAndRemove({ _id: req.user.id });
+
+    res.json({ msg: 'User deleted' });
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server Error');
+  }
+});
 module.exports = router;
